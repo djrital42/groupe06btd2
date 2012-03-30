02e0823bfaf8f2d5de5d698d497c87dd61364fce
Cayuela Christophe <djrital42@hotmail.fr>
2012-03-30 17:32:05 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+"""Test Git"""
+from operators import add, mul
+from logic import and_, or_
+
+def main() :
+    """fonction main"""
+
+    print "My application starts..."
+    a = 2
+    b = 3
+    x = True
+    y = False
+    print add(a, b)
+    print mul(x, y)
+    print and_(x, y)
+    print or_(a, b)
+    
+if __name__ == "__main__" :
+    main()

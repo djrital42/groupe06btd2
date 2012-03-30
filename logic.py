226f0b4e95619fb8acf1ffe9e27c83a3e3324f76
Cayuela Christophe <djrital42@hotmail.fr>
2012-03-30 17:55:46 +0200

    main.py

---
+++ logic.py
@@ -0,0 +1,9 @@
+"""Module logic"""
+
+
+def and_(i, j):
+    """fonction and_"""
+
+
+def or_(k, l):
+    """fonction or_"""
---
+++ operators.py
@@ -0,0 +1,9 @@
+"""Module operators"""
+
+
+def add(i, j):
+    """fonction add"""
+
+
+def mul(k, l):
+    """fonction multiplier"""
